fe0b8e58396e79dd4d5cfb3e9043b42ba754af88
Ansharora124 <ansharora13b@gmail.com>
2025-08-20 12:53:29 +0530

    made local storage for cart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,7 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart=[{
 productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
 quantity : 2,
 
@@ -7,6 +10,13 @@ quantity : 2,
   quantity:1,
 
 }];
+
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 
 export function addToCart(productId){
@@ -24,6 +34,8 @@ export function addToCart(productId){
       quantity:1
     });
   }
+saveToStorage();
+
 }
 
 export function removeFromCart(productId){
@@ -35,4 +47,5 @@ export function removeFromCart(productId){
     }
   });
   cart = newcart;
+  saveToStorage();
 };
